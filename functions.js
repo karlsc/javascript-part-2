319b14ded9d7d674a55055cff25ef1a223b402d8
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-14 19:42:28 +0000

    Added functions that sort by longest name and alpha email

--- functions.js
+++ functions.js
@@ -10,3 +10,30 @@ function sortLongestString(a,b){
 var yourArray = ["Allo","Bonjour"];
 
 yourArray.sort(sortLongestString);
+
+// Function that will sort array by longest name, then longest e-mail
+
+function sortLongestName(a,b){
+    
+    return (b.name).length - (a.name).length ;
+}
+
+function sortAlphaEmail(a,b){
+    
+    return (a.email).localeCompare(b.email) ;
+}
+
+var yourArray = [   {name: "Karl", email: "csdf@hotmail.com"},
+                    {name: "Jack", email: "dsdf@hot.com"},
+                    {name: "Pat", email: "asdf_as@hotmail.com"},
+                    {name: "Thomas", email: "zsdf__34@gmail.com"},
+                    {name: "Johnny-O", email: "xsdf.askd@hotmail666.com"} ];
+                    
+var yourArray2 = [  {name: "Karl", email: "csdf@hotmail.com"},
+                    {name: "Jack", email: "dsdf@hot.com"},
+                    {name: "Pat", email: "asdf_as@hotmail.com"},
+                    {name: "Thomas", email: "zsdf__34@gmail.com"},
+                    {name: "Johnny-O", email: "xsdf.askd@hotmail666.com"} ];
+
+console.log(yourArray.sort(sortLongestName));
+console.log(yourArray2.sort(sortAlphaEmail));
