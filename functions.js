8b37bb836e8ec1728deaa29c0a3a2ba6aac85378
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-14 18:54:31 +0000

    Created function to sort strings by longest to smallest

--- functions.js
+++ functions.js
@@ -1,1 +1,12 @@
 // This file was created for javascript part 2 class
+
+// First exercice: Create an array prototype to sort two strings by longest
+
+function sortLongestString(a,b){
+    
+    return b.length - a.length;
+}
+
+var yourArray = ["Allo","Bonjour"];
+
+yourArray.sort(sortLongestString);
